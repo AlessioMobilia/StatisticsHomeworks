4c2b6987358279c0c4fca8b4807d0b0d9bab3dbc
Alessio Mobilia <mobilia.alessio@gmail.com>
2020-10-19 14:25:10 +0200

    Homework 6A

---
+++ 6A/NaiveArithmeticMean/NaiveArithmeticMean/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace NaiveArithmeticMean
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTest1 = new System.Windows.Forms.Button();
+            this.TextBoxResult = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonTest1
+            // 
+            this.buttonTest1.Location = new System.Drawing.Point(13, 13);
+            this.buttonTest1.Name = "buttonTest1";
+            this.buttonTest1.Size = new System.Drawing.Size(75, 23);
+            this.buttonTest1.TabIndex = 0;
+            this.buttonTest1.Text = "Test1";
+            this.buttonTest1.UseVisualStyleBackColor = true;
+            this.buttonTest1.Click += new System.EventHandler(this.buttonTest1_Click);
+            // 
+            // TextBoxResult
+            // 
+            this.TextBoxResult.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TextBoxResult.Location = new System.Drawing.Point(13, 43);
+            this.TextBoxResult.Name = "TextBoxResult";
+            this.TextBoxResult.Size = new System.Drawing.Size(775, 395);
+            this.TextBoxResult.TabIndex = 1;
+            this.TextBoxResult.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Test2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(177, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Test3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(259, 14);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Test4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(341, 14);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Test5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.TextBoxResult);
+            this.Controls.Add(this.buttonTest1);
+            this.Name = "Form1";
+            this.Text = "6A - Alessio Mobilia";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTest1;
+        private System.Windows.Forms.RichTextBox TextBoxResult;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 6A/NaiveArithmeticMean/NaiveArithmeticMean/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NaiveArithmeticMean
+{
+    public partial class Form1 : Form
+    {
+        public List<List<Double>> datasetlist = new List<List<Double>>();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void buttonTest1_Click(object sender, EventArgs e)
+        {
+            this.TextBoxResult.AppendText("----------------------------------------------\n");
+            this.TextBoxResult.AppendText("TEST 1\n");
+            List < Double > dataset = new List<Double>();
+
+            dataset.Add(20);
+            dataset.Add(20);
+
+            PrintTest(dataset);
+            
+            
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            this.TextBoxResult.AppendText("----------------------------------------------\n");
+            this.TextBoxResult.AppendText("TEST 2\n");
+            List<Double> dataset = new List<Double>();
+
+            dataset.Add(1E300);
+            dataset.Add(10);
+            dataset.Add(10);
+            dataset.Add(-1E300);
+
+            PrintTest(dataset);
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            this.TextBoxResult.AppendText("----------------------------------------------\n");
+            this.TextBoxResult.AppendText("TEST 3\n");
+            List<Double> dataset = new List<Double>();
+
+            dataset.Add(10.2);
+            dataset.Add(-10.1);
+
+            PrintTest(dataset);
+
+        }
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            this.TextBoxResult.AppendText("----------------------------------------------\n");
+            this.TextBoxResult.AppendText("TEST 4\n");
+            List<Double> dataset = new List<Double>();
+
+            dataset.Add(1.0);
+            dataset.Add(1.110223E-16);
+            dataset.Add(-1.110223E-16);
+
+            PrintTest(dataset);
+
+
+
+        }
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            this.TextBoxResult.AppendText("----------------------------------------------\n");
+            this.TextBoxResult.AppendText("TEST 4\n");
+            List<Double> dataset = new List<Double>();
+
+            dataset.Add(10000.0);
+            dataset.Add(3.14159);
+            dataset.Add(2.71828);
+
+            PrintTest(dataset);
+
+        }
+
+
+
+
+
+
+        private void PrintTest(List<double> dataset)
+        {
+            this.TextBoxResult.Clear();
+
+            this.TextBoxResult.AppendText("------------ NAIVE ------------\n");
+            double count = 0;
+            foreach (double data in dataset)
+            {
+                count++;
+                this.TextBoxResult.AppendText("n. " + count.ToString().PadRight(10) + data.ToString() + "\n");
+            }
+            double avg = Statistics.NaiveArithmeticMean(dataset);
+            double sum = Statistics.NaiveSum(dataset);
+            this.TextBoxResult.AppendText("\n");           
+            this.TextBoxResult.AppendText("Sum".PadRight(13) + sum + "\n");
+            this.TextBoxResult.AppendText("Mean".PadRight(13) + avg + "\n");
+            this.TextBoxResult.AppendText("------------------------\n");
+
+
+
+            this.TextBoxResult.AppendText("------------ KNUTH ------------\n");
+            count = 0;
+            Statistics stat = new Statistics();
+            foreach (double data in dataset)
+            {
+                count++;
+                this.TextBoxResult.AppendText("n. " + count.ToString().PadRight(10) + data.ToString() + "\n");
+                stat.OnlineArithmeticMean(data);
+            }
+            avg = stat.avg;
+            this.TextBoxResult.AppendText("\n");
+            this.TextBoxResult.AppendText("Mean".PadRight(13) + avg + "\n");
+            this.TextBoxResult.AppendText("------------------------\n");
+
+
+
+            this.TextBoxResult.AppendText("------------ KAHAN ------------\n");
+            count = 0;
+            foreach (double data in dataset)
+            {
+                count++;
+                this.TextBoxResult.AppendText("n. " + count.ToString().PadRight(10) + data.ToString() + "\n");
+            }
+            avg = Statistics.KahanArithmeticMean(dataset);
+            sum = Statistics.KahanSum(dataset);
+            this.TextBoxResult.AppendText("\n");
+            this.TextBoxResult.AppendText("Sum".PadRight(13) + sum + "\n");
+            this.TextBoxResult.AppendText("Mean".PadRight(13) + avg + "\n");
+            this.TextBoxResult.AppendText("------------------------\n");
+
+
+            this.TextBoxResult.AppendText("------------ NEUMAIER ------------\n");
+            count = 0;
+            foreach (double data in dataset)
+            {
+                count++;
+                this.TextBoxResult.AppendText("n. " + count.ToString().PadRight(10) + data.ToString() + "\n");
+            }
+            avg = Statistics.NeumaierArithmeticMean(dataset);
+            sum = Statistics.NeumaierSum(dataset);
+            this.TextBoxResult.AppendText("\n");
+            this.TextBoxResult.AppendText("Sum".PadRight(13) + sum + "\n");
+            this.TextBoxResult.AppendText("Mean".PadRight(13) + avg + "\n");
+            this.TextBoxResult.AppendText("------------------------\n");
+
+
+
+
+            this.TextBoxResult.AppendText("------------ ARRAY METHODS ------------\n");
+            count = 0;
+            foreach (double data in dataset)
+            {
+                count++;
+                this.TextBoxResult.AppendText("n. " + count.ToString().PadRight(10) + data.ToString() + "\n");
+            }
+            var arr = dataset.ToArray();
+            avg = arr.Average();
+            sum = arr.Sum();
+            this.TextBoxResult.AppendText("\n");
+            this.TextBoxResult.AppendText("Sum".PadRight(13) + sum + "\n");
+            this.TextBoxResult.AppendText("Mean".PadRight(13) + avg + "\n");
+            this.TextBoxResult.AppendText("------------------------\n");
+
+
+
+
+        }
+
+
+
+
+    }
+}
