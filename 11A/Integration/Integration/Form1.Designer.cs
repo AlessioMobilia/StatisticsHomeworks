1583a6ede529d228e737d413a8ffce9d71c6d818
Alessio Mobilia <mobilia.alessio@gmail.com>
2020-11-10 18:04:27 +0100

    homework 5

---
+++ 11A/Integration/Integration/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Integration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelRiemann = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelLebesgue = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelIntervals = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelXMin = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelXMax = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(969, 568);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 622);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Riemann:";
+            // 
+            // labelRiemann
+            // 
+            this.labelRiemann.AutoSize = true;
+            this.labelRiemann.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRiemann.Location = new System.Drawing.Point(191, 624);
+            this.labelRiemann.Name = "labelRiemann";
+            this.labelRiemann.Size = new System.Drawing.Size(18, 16);
+            this.labelRiemann.TabIndex = 3;
+            this.labelRiemann.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(587, 622);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Lebesgue:";
+            // 
+            // labelLebesgue
+            // 
+            this.labelLebesgue.AutoSize = true;
+            this.labelLebesgue.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLebesgue.Location = new System.Drawing.Point(691, 624);
+            this.labelLebesgue.Name = "labelLebesgue";
+            this.labelLebesgue.Size = new System.Drawing.Size(18, 16);
+            this.labelLebesgue.TabIndex = 5;
+            this.labelLebesgue.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(115, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "f(x)=x^2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(430, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Intervals:";
+            // 
+            // labelIntervals
+            // 
+            this.labelIntervals.AutoSize = true;
+            this.labelIntervals.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIntervals.Location = new System.Drawing.Point(544, 15);
+            this.labelIntervals.Name = "labelIntervals";
+            this.labelIntervals.Size = new System.Drawing.Size(18, 16);
+            this.labelIntervals.TabIndex = 8;
+            this.labelIntervals.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(231, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "x:";
+            // 
+            // labelXMin
+            // 
+            this.labelXMin.AutoSize = true;
+            this.labelXMin.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelXMin.Location = new System.Drawing.Point(260, 18);
+            this.labelXMin.Name = "labelXMin";
+            this.labelXMin.Size = new System.Drawing.Size(15, 13);
+            this.labelXMin.TabIndex = 10;
+            this.labelXMin.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(281, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "-->";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // labelXMax
+            // 
+            this.labelXMax.AutoSize = true;
+            this.labelXMax.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelXMax.Location = new System.Drawing.Point(318, 18);
+            this.labelXMax.Name = "labelXMax";
+            this.labelXMax.Size = new System.Drawing.Size(15, 13);
+            this.labelXMax.TabIndex = 12;
+            this.labelXMax.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 658);
+            this.Controls.Add(this.labelXMax);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.labelXMin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelIntervals);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelLebesgue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelRiemann);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelRiemann;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelLebesgue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelIntervals;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelXMin;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelXMax;
+    }
+}
+
---
+++ 11A/Integration/Integration/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Integration
+{
+    public partial class Form1 : Form
+    {
+
+        public Charts chart = new Charts();
+        public Charts chart2 = new Charts();
+        Bitmap b;
+        Graphics g;
+
+        Double minX = 0;
+        Double maxX = 20;
+        
+        Double minY = 0;
+        Double maxY = 400;
+
+        Double step = 0.1;
+        int interval = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.b = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            this.g = Graphics.FromImage(b);
+            this.g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+            labelXMax.Text = maxX.ToString();
+            labelXMin.Text = minX.ToString();
+
+            //initialize the chart
+            chart.minX = minX;
+            chart.minY = minY;
+            chart.maxX = maxX;
+            chart.maxY = maxY;
+
+            chart.viewPort = new Rectangle(100,10,300,500);
+
+
+            chart2.minX = minX;
+            chart2.minY = minY;
+            chart2.maxX = maxX;
+            chart2.maxY = maxY;
+
+            chart2.viewPort = new Rectangle(570, 10, 300, 500);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.g.Clear(Color.White);
+            //draw chart
+            UpdateCharts();
+
+            timer1.Start();
+
+
+
+
+        }
+
+        private void UpdateCharts()
+        {
+            labelIntervals.Text = interval.ToString();
+            chart.DrawViewport(g);
+            chart.DrawAxis(g);
+            chart.DrawFunction(function, minX, maxX, step, g);
+            chart2.DrawViewport(g);
+            chart2.DrawAxis(g);
+            chart2.DrawFunction(function, minX, maxX, step, g);
+            List<Interval> rectangles = NaiveIntegral(function, minX, maxX, interval, chart);
+            chart.DrawHistogram(rectangles,rectangles.Count,g);
+            List<Interval> rectanglesY = NaiveIntegralLebesgue(functionInverse, minY, maxY, interval, chart);
+            chart2.DrawHorizzontalRectangles(rectanglesY, g);
+            pictureBox1.Image = b;
+        }
+
+        public List<Interval> NaiveIntegral(Func<double, double> f, double minX, double maxX, int intervalNumber, Charts chart)
+        {
+            double x = minX;
+            double sum = 0;
+            double y, x2, b, X, area,h;
+            double step = (maxX - minX) / intervalNumber;
+            List<Interval> rectangles = new List<Interval>();
+
+            do
+            {
+                x2 = x + step;
+                b = x2 - x;
+                X = x + (step/2) ;
+                h =  f(X);
+                area = b * h;
+                sum += area;
+                rectangles.Add(new Interval(x,x2, (int)(h*chart.viewPort.Height/chart.maxY)));
+                x += step;
+                
+
+            } while (x2 <= maxX);
+
+            this.labelRiemann.Text = sum.ToString();
+            return rectangles;
+        }
+
+        public List<Interval> NaiveIntegralLebesgue(Func<double, double> f, double minY, double maxY, int intervalNumber, Charts chart)
+        {
+            double y = minY;
+            double sum = 0;
+            double x, y2, b, Y, area, h;
+            double step = (maxY - minY) / intervalNumber;
+            List<Interval> rectangles = new List<Interval>();
+
+            do
+            {
+                y2 = y + step;
+                h = y2 - y;
+                Y = y + (step/2);
+                b = maxX - f(Y);
+                area = b * h;
+                sum += area;
+                rectangles.Add(new Interval(y, y2, (int)(f(Y) * chart.viewPort.Width / chart.maxX)));
+                y += step;
+
+
+            } while (y2 <= maxY);
+
+            this.labelLebesgue.Text = sum.ToString();
+            return rectangles;
+        }
+
+
+
+        private double function(double x)
+        {
+            return Math.Pow(x,2);
+        }
+
+        private double functionInverse(double y)
+        {
+            return Math.Sqrt(y);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.g.Clear(Color.White);
+            interval += interval;
+            UpdateCharts();
+            if (interval >= 1000)
+            {
+                interval = 5;
+                timer1.Stop();
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
